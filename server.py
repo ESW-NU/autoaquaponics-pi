36c3def30d96792b20ad5ea37dce1f8e8b5c8141
andrechen77 <101299705+andrechen77@users.noreply.github.com>
2025-01-26 17:28:07 -0600

    server module logs to its own logger

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 from main import Task
-from logs import global_logger
+from logs import global_logger, setup_logger
+
+server_logger = setup_logger("logs/server.log", "HTTP Server")
 
 class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         # Add CORS header before finishing headers
         self.send_header("Access-Control-Allow-Origin", "*")
         super().end_headers()
+
+    def log_message(self, format, *args):
+        server_logger.info(format % args)
 
 class Server(Task):
     def __init__(self, port=8080):
