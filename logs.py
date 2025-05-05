74274d8ac02066f3244b91ef4c30e53af70e0d18
andrechen77 <101299705+andrechen77@users.noreply.github.com>
2025-05-05 18:15:15 -0500

    logs.py update

--- logs.py
+++ logs.py
@@ -5,23 +5,23 @@ import logging.handlers
 This module sets up logging for the AutoAquaponics system.
 
 # Function
-    `setup_logger(log_file, subsystem_name)`
+    `register_logger(log_file, subsystem_name)`
         Configures and returns a logger for a given subsystem.
 
 # Variables
-    - `_global_log_file: str`: The default log file name for the global logger.
     - `global_logger: logging.Logger`: The global logger instance for the
     AutoAquaponics system. This is instantiated when this module is first
     imported, and other modules can import this logger and use it to log
     messages.
 """
 
-def setup_logger(log_file, subsystem_name):
+formatter = logging.Formatter(
+    "%(asctime)s %(levelname)s in module `%(module)s`: %(message)s",
+)
+
+def register_logger(log_file, subsystem_name):
     logger = logging.getLogger(subsystem_name)
     logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        "%(asctime)s %(levelname)s in module `%(module)s`: %(message)s",
-    )
 
     handler = logging.handlers.TimedRotatingFileHandler(
         log_file,
@@ -33,6 +33,6 @@ def setup_logger(log_file, subsystem_name):
 
     return logger
 
-_global_log_file = "logs/autoaquaponics.log"
-global_logger = setup_logger(_global_log_file, "AutoAquaponics System")
+global_logger = register_logger("logs/global.log", "AutoAquaponics System")
+pykka_logger = register_logger("logs/pykka.log", "pykka") # pykka uses the `pykka` logger name
 global_logger.info("logger setup complete")
